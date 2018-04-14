a96d44840f4b33fabd94c3f79a176151f9c41637
Hussein El Kheshen <husseinelkheshen@gmail.com>
2018-04-13 20:50:11 -0500

    Back button on Login Screen Functional

--- AndroidQuiz/app/src/main/java/com/husseinelkheshen/quiz/Login.java
+++ AndroidQuiz/app/src/main/java/com/husseinelkheshen/quiz/Login.java
@@ -1,13 +1,78 @@
 package com.husseinelkheshen.quiz;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+    private Button login;
+    private Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        login = findViewById(R.id.login);
+        back = findViewById(R.id.back);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v == login){
+                    LoginUser();
+                }
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v == back){
+                    startActivity(new Intent(getApplicationContext(),
+                            Register.class));
+                }
+            }
+        });
+    }
+
+    public void LoginUser(){
+        String Email = email.getText().toString().trim();
+        String Password = password.getText().toString().trim();
+        mAuth.signInWithEmailAndPassword(Email, Password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            currentUser = mAuth.getCurrentUser();
+                            finish();
+                            startActivity(new Intent(getApplicationContext(),
+                                    Menu.class));
+                        }else {
+                            Toast.makeText(Login.this, "couldn't login",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
