f19b566e128fdec2da4cf1290cc0cd584c20457f
Hussein El Kheshen <husseinelkheshen@gmail.com>
2018-04-13 19:58:29 -0500

    Adding Login Screen

--- AndroidQuiz/app/src/main/java/com/husseinelkheshen/quiz/Menu.java
+++ AndroidQuiz/app/src/main/java/com/husseinelkheshen/quiz/Menu.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.firebase.auth.FirebaseAuth;
-
 public class Menu extends AppCompatActivity {
 
 
---
+++ AndroidQuiz/app/src/main/java/com/husseinelkheshen/quiz/Register.java
@@ -0,0 +1,89 @@
+package com.husseinelkheshen.quiz;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Register extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText password;
+    private EditText email;
+    private Button register;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        register = findViewById(R.id.register);
+        login = findViewById(R.id.login);
+        mAuth = FirebaseAuth.getInstance();
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v == register){
+                    RegisterUser();
+                }
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v == login){
+                    startActivity(new Intent(getApplicationContext(),
+                            Login.class));
+                }
+            }
+        });
+    }
+
+    public void RegisterUser() {
+        String Email = email.getText().toString().trim();
+        String Password = password.getText().toString().trim();
+        if (TextUtils.isEmpty(Email)) {
+            Toast.makeText(this, "A Field is Empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(Password)) {
+            Toast.makeText(this, "A Field is Empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(Email, Password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        try {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(Register.this, "registration successful",
+                                        Toast.LENGTH_SHORT).show();
+                                finish();
+                                startActivity(new Intent(getApplicationContext(), Menu.class));
+                            } else {
+                                Toast.makeText(Register.this, "Couldn't register, try again",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+    }
+}
