0b03483f8973425a10e20dafdfbf48c7489308ee
Hussein El Kheshen <husseinelkheshen@gmail.com>
2018-04-23 11:21:46 -0500

    Trying to figure out Graceful FB Logout

--- AndroidQuiz/app/src/main/java/com/husseinelkheshen/elgayza/Menu.java
+++ AndroidQuiz/app/src/main/java/com/husseinelkheshen/elgayza/Menu.java
@@ -6,10 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.facebook.AccessToken;
+import com.facebook.login.LoginManager;
+
 public class Menu extends AppCompatActivity {
 
 
-    Button playButton, quitButton;
+    Button playButton, quitButton, logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,16 @@ public class Menu extends AppCompatActivity {
 
         playButton = findViewById(R.id.play);
         quitButton = findViewById(R.id.quit);
+        logout = findViewById(R.id.logout);
+
+        if (AccessToken.getCurrentAccessToken() == null) {
+            startActivity(new Intent(getApplicationContext(),
+                    Register.class));
+            finish();
+        }
+        else {
+            logout.setVisibility(View.VISIBLE);
+        }
 
         playButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -30,11 +43,24 @@ public class Menu extends AppCompatActivity {
                 finish();
             }
         });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+                startActivity(new Intent(getApplicationContext(), Register.class));
+                finish();
+            }
+        });
     }
 
     public void playQuiz()
     {
         startActivity(new Intent(getApplicationContext(), Quiz.class));
+    }
+
+    public void logout() {
+        LoginManager.getInstance().logOut();
     }
 }
 
