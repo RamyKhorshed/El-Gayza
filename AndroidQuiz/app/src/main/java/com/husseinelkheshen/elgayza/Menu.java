72c4e94b0fc21b33068d5039a9c8315fa991f7c9
Hussein El Kheshen <husseinelkheshen@gmail.com>
2018-04-25 14:08:02 -0500

    Fixed Graceful FB Logout

--- AndroidQuiz/app/src/main/java/com/husseinelkheshen/elgayza/Menu.java
+++ AndroidQuiz/app/src/main/java/com/husseinelkheshen/elgayza/Menu.java
@@ -1,20 +1,25 @@
 package com.husseinelkheshen.elgayza;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
 import com.facebook.login.LoginManager;
 import com.facebook.login.widget.LoginButton;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Menu extends AppCompatActivity {
 
 
     Button playButton, quitButton;
     LoginButton loginButton;
+
+    private CallbackManager callbackManager;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +28,21 @@ public class Menu extends AppCompatActivity {
 
         playButton = findViewById(R.id.play);
         quitButton = findViewById(R.id.quit);
+        loginButton = findViewById(R.id.login_button);
 
-        if (AccessToken.getCurrentAccessToken() == null) {
-            startActivity(new Intent(getApplicationContext(),
-                    Register.class));
-            finish();
-        }
+        callbackManager = CallbackManager.Factory.create();
 
         playButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 playQuiz();
+            }
+        });
+
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mAuth.getInstance().signOut();
+                LoginManager.getInstance().logOut();
+                finish();
             }
         });
 
